d8468d018ecb37baeaaecf3bb29b5e0af8be546a
André Rodrigues de Melo <andrermelo1@hotmail.com>
2021-08-15 16:15:47 -0300

    autenticação com firebase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,7 @@
+import { useContext } from 'react';
 import { useHistory } from 'react-router-dom'
+
+import { AuthContext } from '../App';
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg';
@@ -10,9 +13,16 @@ import '../styles/auth.scss';
 
 export function Home() {
     const history = useHistory();
-    
-    function navigateToNewRoom() {
+    const { user, signInWithGoogle } = useContext(AuthContext)
+
+    async function handleCreateRoom() {
+
+        if (!user) {
+            await signInWithGoogle()
+        }
+
         history.push('/rooms/new')
+        
     }
     
     return (
@@ -25,7 +35,7 @@ export function Home() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                    <button onClick={navigateToNewRoom} className="create-room">
+                    <button onClick={handleCreateRoom} className="create-room">
                         <img src={googleIconImg} alt="Logo da Google" />
                         Crie sua sala com o google
                     </button>
